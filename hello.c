79ae7145f06a41bac9dfa96380a97acf748c5c6b
Jake Moseley <jmoseley2@huskers.unl.edu>
2022-08-26 17:50:20 +0000

    Added my major and name to output

--- hello.c
+++ hello.c
@@ -6,6 +6,9 @@ int main(int argc, char **argv) {
     printf("Maximilian Clark!\n");
     printf("Electrical Engineering\n");
 
+    printf("Jacob Moseley\n");
+    printf("Electrical Engineering\n");
+
     return 0;
 
 }
